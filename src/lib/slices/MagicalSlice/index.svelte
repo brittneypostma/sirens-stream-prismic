41cbee7b4bb7f6449b7badabb2d7a28f913e10c2
brittneypostma <bdesignedwebdev@gmail.com>
2023-09-13 13:53:25 -0400

    init

---
+++ src/lib/slices/MagicalSlice/index.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { PrismicImage, PrismicRichText } from '@prismicio/svelte';
+
+	/** @type {import("@prismicio/client").Content.MagicalSliceSlice} */
+	export let slice;
+</script>
+
+<section data-slice-type={slice.slice_type} data-slice-variation={slice.variation}>
+	<PrismicRichText field={slice.primary.rich_text} />
+	<PrismicImage field={slice.primary.image} />
+</section>
+
+<style>
+	section :global(img) {
+		width: 200px;
+		height: 200px;
+	}
+</style>
